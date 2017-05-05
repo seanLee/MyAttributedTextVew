b6d6104fbd1d5109d30973c1eb3ee079bf70e7fb
Sean <seannnLee@163.com>
2017-05-05 22:11:50 +0800

    Initial Commit

---
+++ TestTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTextView
+//
+//  Created by Sean on 2017/5/5.
+//  Copyright © 2017年 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
